f6906463d8091d41a0c5237c2678afc3db4616ea
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-05-30 13:41:50 +0530

    Add files via upload

---
+++ src/Component/Data/index.js
@@ -0,0 +1,224 @@
+const products = [
+  {
+    id: 1,
+    name: "Brown Shirt",
+    description: "Brown T-Shirt for Women",
+    price: 16.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/3YJNTzX/1-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 2,
+    name: "Light Brown Shirt",
+    description: "Light Brown Shirt for Women",
+    price: 4.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/wwPhH8X/2-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 3,
+    name: "Women Grey Shirt",
+    description: "Grey Shirt for Women",
+    price: 14.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/T4xG0Tr/3-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 4,
+    name: "Warm Shirt Women",
+    description: "Woolen Hoodie Women",
+    price: 20.00,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/GxhWT45/4-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 5,
+    name: "Women Grey Shirt",
+    description: "Light Grey Shirt for Women",
+    price: 4.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/YktKwbH/5-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 6,
+    name: "Women Red/Brown Shirt",
+    description: "Red/Brown Blouse for Women",
+    price: 19.99,
+    gender: "women",
+    type: "blouse",
+    img:"https://i.ibb.co/3h0B8JS/6-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 7,
+    name: "Dark Grey Shirt Women",
+    description: "Dark Grey Shirt for Women",
+    price: 6.00,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/W6m7vrM/7-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 8,
+    name: "White Shirt Women",
+    description: "White Shirt for Women",
+    price: 14.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/R2BVrHt/8-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 9,
+    name: "Black Shirt Women",
+    description: "Black Shirt for Women",
+    price: 20.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/fX4JvQV/10-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 10,
+    name: "No Shoulder Hoodie",
+    description: "Hoodie for Women",
+    price: 4.99,
+    gender: "women",
+    type: "shirt",
+    img:"https://i.ibb.co/jvzjVWT/9-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 11,
+    name: "Women Watch Gold",
+    description: "Golden Watch for Women",
+    price: 45.99,
+    gender: "women",
+    type: "watch",
+    img:"https://i.ibb.co/cCJsYDT/11-jpg.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 12,
+    name: "Black Pearl Necklace",
+    description: "Black Pearl Necklace for Women",
+    price: 14.99,
+    gender: "women",
+    type: "necklace",
+    img:"https://i.ibb.co/7ndkbfC/12-jpg.jpg" ,
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 13,
+    name: "Man Black Shirt",
+    description: "Black T-Shirt for Men",
+    price: 10.99,
+    gender: "men",
+    type: "shirt",
+    img:"https://i.ibb.co/hgwZLWF/13-jpg.jpg" ,
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 14,
+    name: "Man Grey Tanktop",
+    description: "Grey Tanktop for Men",
+    price: 14.99,
+    gender: "men",
+    type: "shirt",
+    img:"https://i.ibb.co/Vp11tGr/14-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 15,
+    name: "Man White Shirt",
+    description: "White Shirt for Men",
+    price: 20.99,
+    gender: "men",
+    type: "shirt",
+    img:"https://i.ibb.co/wM44WY8/15-jpg.jpg" ,
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 16,
+    name: "Man Brown Shirt",
+    description: "Brown Shirt for Men",
+    price: 13.99,
+    gender: "men",
+    type: "shirt",
+    img:"https://i.ibb.co/6srfCfg/16-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 17,
+    name: "Man Black shoes",
+    description: "Black shoes for Men",
+    price: 13.99,
+    gender: "men",
+    type: "shoes",
+    img:"https://i.ibb.co/jRxdrw4/17-jpg.jpg" ,
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 18,
+    name: "Black Shirt Men",
+    description: "Black Shirt for Men",
+    price: 9.99,
+    gender: "men",
+    type: "shirt",
+    img:"https://i.ibb.co/KXbgWtj/18-jpg.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 19,
+    name: "4-Pack Man Ties",
+    description: "Ties for Men",
+    price: 35.99,
+    gender: "men",
+    type: "tie",
+    img:"https://i.ibb.co/CV8hVJ6/19-jpg.png",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 20,
+    name: "Man Black Tie",
+    description: "Black Tie for Men",
+    price: 15.99,
+    gender: "men",
+    type: "tie",
+    img:"https://i.ibb.co/vVnzFHn/20-jpg.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+];
+
+export default products;
